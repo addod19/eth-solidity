6afab58e07da919d00fe211f60ad2f1459bf855b
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2022-01-09 01:58:50 +0000

    Improve Welcome form position on sm device

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,6 +1,8 @@
 const Loader = () => {
   return (
-    <h1>Loader</h1>
+    <div className="flex justify-center items-center mt-10 pt-5">
+      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-red-700"></div>
+    </div>
   )
 }
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -31,8 +31,8 @@ const Welcome = () => {
   }
   return (
     <div className="flex w-full justify-content items-center">
-      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col md:mr-10">
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send CRYPTO <br/> across the world
           </h1>
@@ -68,7 +68,7 @@ const Welcome = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
           <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorpism">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
@@ -87,14 +87,14 @@ const Welcome = () => {
               </div>
             </div>
           </div>
-          <div className="p-5 sm:w-96 w-4 flexflex-col- justify-start items-center blue-glassmorphism">
+          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
             <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
             <Input placeholder="Amount (Eth)" name="amount" type="number" handleChange={() => {}} />
             <Input placeholder="Keyword (GiF)" name="keyword" type="text" handleChange={() => {}} />
             <Input placeholder="Enter the message" name="message" type="text" handleChange={() => {}} />
 
             <div className="h-[1px] w-full bg-gray-400 my-2">
-            {false? (
+            {true? (
               <Loader />
             ): (
               <button
